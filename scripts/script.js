5eccf5ff356a6d5487e54a37e3757aff40759b42
Mia Adjei <adjei.m@gmail.com>
2016-11-27 23:34:07 -0500

    Change method of adding new songs from push to unshift.

--- scripts/script.js
+++ scripts/script.js
@@ -22,6 +22,7 @@ var loadButton = document.querySelector(".loadButton");
 var nextButton = document.querySelector(".nextButton");
 
 
+
 function Jukebox() {
 	this.songList = [];
 	this.songFile = document.createElement("audio");
@@ -36,8 +37,13 @@ function Jukebox() {
 	// // Displays the current song and a UI for the player
 
 	this.addSong = function(newSong) {
-		this.songList.push(newSong);
+		this.songList.unshift(newSong);
 		this.songFile.src = this.songList[0].url;
+
+		this.songFile.title = this.songList[0].title;
+		this.songFile.artist = this.songList[0].artist;
+		songTitle.innerText = this.songFile.title;
+		songArtist.innerText = this.songFile.artist;
 	}
 
 	this.play = function() {
@@ -58,15 +64,17 @@ function Jukebox() {
 		this.newSongTitle = prompt("Enter the title of your song.");
 		this.newSongArtist = prompt("Enter the artist of your song.");
 		this.newSongUrl = prompt("Enter the url of your song.")
-		this.songList.push(new Song(this.newSongTitle, this.newSongArtist, this.newSongUrl));
+		this.songList.unshift(new Song(this.newSongTitle, this.newSongArtist, this.newSongUrl));
 
 	}
 
-	// this.currentSong = 
 
 	this.changeSong = function() {
-		this.songFile.setAttribute("src", this.songList[1].url);
-
+		this.songFile.setAttribute("src", this.songList[0].url);
+		this.songFile.title = this.songList[0].title;
+		this.songFile.artist = this.songList[0].artist;
+		songTitle.innerText = this.songFile.title;
+		songArtist.innerText = this.songFile.artist;
 
 
 	}
@@ -84,30 +92,6 @@ function Jukebox() {
 	// });
 	// So far, can't seem to control the actions triggered by the button clicks from inside the Jukebox object.
 
-
-	// songTitle = 
-	// this.showSongTitle = function() {
-	// 	for (var i = 0; i < this.songList.length; i ++) {
-	// 		this.songTitle = this.songList[i].title;
-	// 	}
-	// }
-	// this.showSongTitle();
-
-	// document.querySelector(".songTitle").innerText = this.songTitle;
-	// songTitle
-	// this.songTitle = document.querySelector(".songTitle");
-	// this.songTitle;
-
-
-	// this.skipForward = function() {
-		
-	// 	//what makes move to the next song
-	// }
-
-
-	// this.skipBackward = function() {
-	// 	//what makes move to the previous song
-	// }
 }
 
 
@@ -119,8 +103,8 @@ function Jukebox() {
 var myJukebox = new Jukebox();
 myJukebox.addSong(tudoPassa);
 
-songTitle.innerText = myJukebox.songList[0].title;
-songArtist.innerText = myJukebox.songList[0].artist;
+// songTitle.innerText = myJukebox.songList[0].title;
+// songArtist.innerText = myJukebox.songList[0].artist;
 
 playButton.addEventListener("click", function(){
 	myJukebox.play();
@@ -142,15 +126,8 @@ nextButton.addEventListener("click", function(){
 	myJukebox.changeSong();
 });
 
-// "assets/05 - Geografía.mp3"
-
-
-//how would you get the title of the new song?
-
-//then add the part about changing songs
-
-// a button that listens for a click
-// hide the normal controls for the html5 player so can make own controls
+// "assets/Geografia.mp3"
+// "assets/Libertango.mp3"
 
 // You will build a music player that will end up playing any mp3 you can find online. Think about what functionality you might need for this to work. Encapsulate all of this functionality in a JavaScript object so that starting a song is as simple as calling Jukebox.play() You need an array of song objects and the ability to play, stop and pause a song. How would you switch songs? Can you shuffle songs? Focus on getting the JavaScript to work and keep the HTML/CSS minimal, make it work before you make it look good!
 
@@ -161,25 +138,3 @@ nextButton.addEventListener("click", function(){
 // Give the user the ability to stop that song without using the "built-in" stop button. Once again, this could be through a different button, through clicking or mousing over an image on the page, or any other device of your choosing.
 // Give the user the ability to load at least one different song into the Jukebox besides the one that is loaded when the page initially renders
 // The whole Jukebox should be backed by an object called Jukebox with methods to play, stop, and load songs.
-
-
-
-//bonus ideas: skip forward, skip backwards, shuffle,
-
-
-//the playlist idea of kind of a bonus maybs? idea so that the user can load a variety of playlists and have them kept separately. probs would work best with a database of some sort attached.
-
-// function Playlist() {
-// 	this.songs = [];
-// 	this.addSong = function(newSong) {
-// 		this.songs.push(newSong);
-// 	}
-// 	//suggestion: add a way for people to name their playlists: takes a user input and saves it
-// }
-
-
-// As a user, I want to:
-// see the names of the songs and how long they are
-// see what genre the song is in
-// add songs to my playlist
-// name my playlist (possibly)
