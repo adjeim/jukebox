bbc01421a676efcc03ee1035d363db615cd5b1a0
Mia Adjei <adjei.m@gmail.com>
2016-11-27 23:40:27 -0500

    Clarify what each function should do.

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,6 @@ function Song(title, artist, url) {
 	this.title = title;
 	this.artist = artist;
 	this.url = url;
-	// this.playTime = playTime;
-	// this.genre = genre;
 
 }
 
@@ -28,13 +26,6 @@ function Jukebox() {
 	this.songFile = document.createElement("audio");
 	// Creates a new audio element.
 
-	// this.currentSong = this.songList[0];
-	// songTitle.innerText = this.currentSong;
-
-	// this.songFile.setAttribute("controls", "controls");
-	// 
-	// this.displaySong = document.body.appendChild(this.songFile);
-	// // Displays the current song and a UI for the player
 
 	this.addSong = function(newSong) {
 		this.songList.unshift(newSong);
@@ -45,6 +36,7 @@ function Jukebox() {
 		songTitle.innerText = this.songFile.title;
 		songArtist.innerText = this.songFile.artist;
 	}
+	// Add songs by unshifting them to the beginning of the array.
 
 	this.play = function() {
 		this.songFile.play();
@@ -67,6 +59,7 @@ function Jukebox() {
 		this.songList.unshift(new Song(this.newSongTitle, this.newSongArtist, this.newSongUrl));
 
 	}
+	// Prompt user to add a new song to the queue. The song shows up when the "next song" button is clicked.
 
 
 	this.changeSong = function() {
@@ -75,7 +68,6 @@ function Jukebox() {
 		this.songFile.artist = this.songList[0].artist;
 		songTitle.innerText = this.songFile.title;
 		songArtist.innerText = this.songFile.artist;
-
 
 	}
 
@@ -96,15 +88,9 @@ function Jukebox() {
 
 
 
-
-
-
-
 var myJukebox = new Jukebox();
 myJukebox.addSong(tudoPassa);
 
-// songTitle.innerText = myJukebox.songList[0].title;
-// songArtist.innerText = myJukebox.songList[0].artist;
 
 playButton.addEventListener("click", function(){
 	myJukebox.play();
@@ -129,12 +115,3 @@ nextButton.addEventListener("click", function(){
 // "assets/Geografia.mp3"
 // "assets/Libertango.mp3"
 
-// You will build a music player that will end up playing any mp3 you can find online. Think about what functionality you might need for this to work. Encapsulate all of this functionality in a JavaScript object so that starting a song is as simple as calling Jukebox.play() You need an array of song objects and the ability to play, stop and pause a song. How would you switch songs? Can you shuffle songs? Focus on getting the JavaScript to work and keep the HTML/CSS minimal, make it work before you make it look good!
-
-// Your JavaScript Jukebox should:
-
-// Display at least one song on the page when the page loads
-// Give the user the ability to play that song, without using the "built-in" play button. This could be through a different button, through clicking or mousing over an image on the page, or any other device of your choosing.
-// Give the user the ability to stop that song without using the "built-in" stop button. Once again, this could be through a different button, through clicking or mousing over an image on the page, or any other device of your choosing.
-// Give the user the ability to load at least one different song into the Jukebox besides the one that is loaded when the page initially renders
-// The whole Jukebox should be backed by an object called Jukebox with methods to play, stop, and load songs.
